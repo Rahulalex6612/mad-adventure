eb937e82c438f4f60f99b55623668b9888b3249e
Rahul Pinnala <pinnala@uni-bremen.de>
2026-03-26 15:46:55 +0100

    added the intro scene for finding the lake

--- scenes/lake_swimming.py
+++ scenes/lake_swimming.py
@@ -1,7 +1,9 @@
 """
 =====================================================================
-  Scenes / Lake Swimming
-  
+  Scenes / Dungen Crawl
+  Author: 	Rahul Pinnala
+  Date: 	26.03.2026
+  Contact:	pinnala@uni-bremen.de
 =====================================================================
 """
 
@@ -15,41 +17,25 @@ from utils import sleep
 @register    # You need to register the entry scene of your arc (and add the module to the scenes.__init__.py file.
 def example_scene():
     
-    print(f"\nYou can write some text like this")
+    print(f"\nit is a sunny day and something shiny caught your eye")
     sleep(2.0) # Add some suspense by adding breaks between text (this would add a 2 seconds break)
-    print("There can also be a break in the same line", end=' ', flush=True)
+    print(f"\n.....**shiny shiny**", end=' ', flush=True)
     sleep(2.0)
-    print("like this!")
-    sleep(1.0)
+    print(f"\nyou realized something is there beyond the very thick high bushes")
+    sleep(2.0)
+	print(f"\nyou go closer towards it squeezing through the bushes", end=' ', flush=True)
+    sleep(2.0)
+	print(f"\nafter passing through enough bushes and you open your eyes")
+	sleep(2.0)
+	print(f"\nyou see a beautiful crystal clear lake infront of you")
+	sleep(2.0)
+	print(f"\ndo you want to swimm ")
 
     # You can add options for the player to choose from like this:
     next_scene = choose_option([
-        ('option 1', continuation_scene), # If you want option 1 to lead to a continuation_scene
-        ('option 2', None)                # If you want option 2 to exit this scene and continue the game
+        ('Yes, i will definetly pick it', continuation_scene), # If you want option 1 to lead to a continuation_scene
+        ('No, its probaly not worth anything', None)                # If you want option 2 to exit this scene and continue the game
     ])
 
     return next_scene
 
-# -----------------------------------------------
-
-def continuation_scene():
-    
-    print(f"\nThis is an example continuation scene")
-    sleep(1.0)
-
-    example_roll = chance_roll(25) # You can use a chance roll like this, where 25 is the probability of success
-    
-    if example_roll:
-        
-        print(f"\nThings are happening!")
-        sleep(1.0)
-
-        return None
-        # When you are ready to end the scene, return:
-        #   None:  to continue the game
-        #   True:  to win the game
-        #   False: to lose the game
-    
-    else:
-
-        return None
